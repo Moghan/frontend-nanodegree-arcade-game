034be3766babacbb80a7d8f23f727cf5e6555bd7
Moghan <anders.kensby@gmail.com>
2016-01-27 18:54:07 +0100

    refactor:	Make inputEngine global.

--- js/app.js
+++ js/app.js
@@ -11,54 +11,48 @@ Entity.prototype.update = function() {
     // which will ensure the game runs at the same speed for
     // all computers.
 
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Entity.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
+
 var pInputEngine = function() {
     this.bindings = {};
     this.actions = {};
 
 // TODO: Option to customize inputkeys
-    this.setupKeys();
-        
+    this.bindKey(87, 'move-up');
+    this.bindKey(83, 'move-down');
+    this.bindKey(65, 'move-left');
+    this.bindKey(68, 'move-right');        
 };
 
-pInputEngine.prototype.setupKeys = function() {
-    this.bindings[87] = 'move-up';
-    this.bindings[83] = 'move-down';
-    this.bindings[65] = 'move-left';
-    this.bindings[68] = 'move-right';
+pInputEngine.prototype.bindKey = function(key, action) {
+    this.bindings[key] = action;
 };
 
 // Player to control
-var Player = function() {
-    this.inputEngine = new pInputEngine();
-
-    x = 100;
-    y = 200;
-
+var Player = function(x, y) {
     Entity.call(this, x, y, 'images/char-boy.png');
+    var snd = new Audio('sounds/river_s-rikkisch-8138_hifi.mp3');
+    snd.play();
 };
 
 
 Player.prototype = Object.create(Entity.prototype);
 Player.prototype.constructor = Player;
 
-var player = new Player();
-console.log('x' + player.x +':sprite = ' + player.sprite);
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     
-    x = 150;
-    y = 150;
     sprite = 'images/enemy-bug.png';
     
 
@@ -66,16 +60,10 @@ var Enemy = function() {
 };
 
 Player.prototype.update = function() {
-    /*
-    if(this.keyState[87]) this.y -= 5;
-    if(this.keyState[83]) this.y += 5;
-    if(this.keyState[65]) this.x -= 5;
-    if(this.keyState[68]) this.x += 5;
-    */
-    if(this.inputEngine.actions['move-up']) this.y -= 5;
-    if(this.inputEngine.actions['move-down']) this.y += 5;
-    if(this.inputEngine.actions['move-left']) this.x -= 5;
-    if(this.inputEngine.actions['move-right']) this.x += 5;
+    if(inputEngine.actions['move-up']) this.y -= 5;
+    if(inputEngine.actions['move-down']) this.y += 5;
+    if(inputEngine.actions['move-left']) this.x -= 5;
+    if(inputEngine.actions['move-right']) this.x += 5;
 };
 
 Enemy.prototype = Object.create(Entity.prototype);
@@ -88,20 +76,24 @@ Enemy.prototype.constructor = Enemy;
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var e1 = new Enemy();
-var e2 = new Enemy();
+var e1 = new Enemy(150, 150);
+var e2 = new Enemy(300, 50);
 
 var allEnemies = [e1,e2];
 // Place the player object in a variable called player
+var player = new Player(200,200);
+console.log('x' + player.x +':sprite = ' + player.sprite);
+
+inputEngine = new pInputEngine();
 
 
 var onKeyUp = function (event) {
-    player.inputEngine.actions[player.inputEngine.bindings[event.keyCode]] = false;
+    inputEngine.actions[inputEngine.bindings[event.keyCode]] = false;
 };
 
 var onKeyDown = function (event) {
-    player.inputEngine.actions[player.inputEngine.bindings[event.keyCode]] = true;
-}
+    inputEngine.actions[inputEngine.bindings[event.keyCode]] = true;
+};
 
 document.addEventListener('keyup', onKeyUp);
 document.addEventListener('keydown', onKeyDown);
